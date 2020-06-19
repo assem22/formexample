fbd4157226a0025a2179b6334f030c6698706eb9
assem22 <assem.mukhamadi@mail.ru>
2020-06-19 12:47:44 +0600

    exp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserService} from './service/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-forms';
+  title = 'angular';
+  userFrom: FormGroup;
+  users: any;
+
+
+  constructor(private formBuilder: FormBuilder, private service: UserService) {
+    this.userFrom = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(5)]],
+      lastName: [''],
+      address1: [''],
+      address2: [''],
+      city: [''],
+      province: [''],
+      country: [''],
+      postalCode: [''],
+      email: ['', [Validators.required, Validators.pattern('"^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+    });
+    this.getUserList();
+
+  }
+
+  getUserList() {
+    console.log(this.userFrom.getRawValue());
+    this.service.get().subscribe(res => {
+      this.users = res;
+    });
+  }
+  createUser() {
+    this.create();
+    this.getUserList();
+  }
+  create() {
+    console.log(this.userFrom.getRawValue());
+    this.service.create(this.userFrom.getRawValue()).subscribe(res => {
+      alert(res.name);
+    });
+  }
+
+  delete(id: number) {
+    this.service.delete(id).subscribe(data => {
+      this.getUserList();
+    });
+  }
+
+  updateUser() {
+    console.log('this.userFrom.getRawValue()');
+    console.log(this.userFrom.getRawValue());
+    this.service.update(this.userFrom.getRawValue()).subscribe(data =>
+      console.log(data), error => console.log(error));
+    this.getUserList();
+  }
+  update(user: any) {
+    this.userFrom = this.formBuilder.group({
+      id: [user.id],
+      name: [user.name, [Validators.required, Validators.minLength(5)]],
+      lastName: [user.lastName],
+      address1: [user.address1],
+      address2: [user.address2],
+      city: [user.city],
+      province: [user.province],
+      country: [user.country],
+      postalCode: [user.postalCode],
+      email: [user.email, [Validators.required, Validators.pattern('"^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
